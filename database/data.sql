c6760b18528eddd7db1e65349bbc73deaa2063b0
lindsay-jin <lindsayjin1@gmail.com>
2024-04-16 20:46:40 +0000

    Initial commit

--- database/data.sql
+++ database/data.sql
@@ -1,11 +1,9 @@
--- Use SQL insert statements to add any
--- starting/dummy data to your database tables
+insert into "category" ("name")
+values ('READY-TO-WEAR'), ('GIFTS'), ('LUMI CLUB');
 
--- EXAMPLE:
+insert into "subcategory" ("name", "categoryId")
+values('Dresses', 1), ('Shirts and tops', 1), ('Outerwear', 1);
 
---  insert into "todos"
---    ("task", "isCompleted")
---    values
---      ('Learn to code', false),
---      ('Build projects', false),
---      ('Get a job', false);
+insert into "product" ("name", "color", "size", "material", "description", "imageUrl", "price", "categoryId", "subcategoryId")
+values ('Utility jacket', 'green', 4, 'nylon', 'Durable nylon utility jacket in green with multiple pockets.', 'server/public/images/jacket2.jpg', 1100, 1, 3),
+('Organza dress', 'white', 2, 'silk', 'Airy and light long dress in silk georgette and organza.', 'server/public/images/dress1.jpg', 750, 1, 1);
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,63 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "user" (
+  "userId" serial PRIMARY KEY,
+  "username" text,
+  "password" text,
+  "createdAt" timestamptz DEFAULT 'now()'
+);
+
+CREATE TABLE "category" (
+  "categoryId" serial PRIMARY KEY,
+  "name" text
+);
+
+CREATE TABLE "subcategory" (
+  "subcategoryId" serial PRIMARY KEY,
+  "name" text,
+  "categoryId" int
+);
+
+CREATE TABLE "product" (
+  "productId" serial PRIMARY KEY,
+  "name" text,
+  "description" text,
+  "color" text,
+  "size" int,
+  "material" text,
+  "imageUrl" text,
+  "price" int,
+  "categoryId" int,
+  "subcategoryId" int
+);
+
+CREATE TABLE "wishlist" (
+  "wishlistId" serial PRIMARY KEY,
+  "userId" int,
+  "productId" int,
+  "createdAt" timestamptz DEFAULT 'now()'
+);
+
+CREATE TABLE "cart" (
+  "cartId" serial PRIMARY KEY,
+  "userId" int,
+  "productId" int,
+  "quantity" int,
+  "createdAt" timestamptz DEFAULT 'now()'
+);
+
+ALTER TABLE "subcategory" ADD FOREIGN KEY ("categoryId") REFERENCES "category" ("categoryId");
+
+ALTER TABLE "product" ADD FOREIGN KEY ("categoryId") REFERENCES "category" ("categoryId");
+
+ALTER TABLE "product" ADD FOREIGN KEY ("subcategoryId") REFERENCES "subcategory" ("subcategoryId");
+
+ALTER TABLE "wishlist" ADD FOREIGN KEY ("userId") REFERENCES "user" ("userId");
+
+ALTER TABLE "wishlist" ADD FOREIGN KEY ("productId") REFERENCES "product" ("productId");
+
+ALTER TABLE "cart" ADD FOREIGN KEY ("userId") REFERENCES "user" ("userId");
+
+ALTER TABLE "cart" ADD FOREIGN KEY ("productId") REFERENCES "product" ("productId");
