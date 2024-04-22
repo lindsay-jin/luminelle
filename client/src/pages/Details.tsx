e28498509f9df28b72f38d3fb63d83687d349a75
lindsay-jin <lindsayjin1@gmail.com>
2024-04-22 20:24:46 +0000

    edited sizes in details page

--- client/src/pages/Details.tsx
+++ client/src/pages/Details.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { FaRegHeart } from 'react-icons/fa6';
 // import { IoMdHeart } from "react-icons/io";
 import { FaCircle } from 'react-icons/fa';
+import { FaRegCircle } from 'react-icons/fa';
 import { toDollars } from '../../lib/to-dollars';
 
 export type ProductDetails = {
@@ -12,53 +13,57 @@ export type ProductDetails = {
   price: number;
   color: string;
   sizes: string[];
-}
+};
 
 export function Details() {
-  const {productId} = useParams<{productId: string}>();
-  const [details, setDetails] = useState();
+  const { productId } = useParams<{ productId: string }>();
+  const [details, setDetails] = useState<ProductDetails | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<unknown>();
 
-  useEffect(()=>{
-    async function loadDetails(){
-      try{
-        const response = await fetch(
-          `/api/p/${productId}`
-        );
-        if(!response.ok) throw new Error(`Fetch error with status ${response.status}`);
+  useEffect(() => {
+    async function loadDetails() {
+      try {
+        const response = await fetch(`/api/p/${productId}`);
+        if (!response.ok)
+          throw new Error(`Fetch error with status ${response.status}`);
         const result = await response.json();
         setDetails(result);
-      }catch(error){
-        setError(error)
-      }finally{
-        setIsLoading(false)
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
       }
     }
     if (productId) {
       loadDetails();
     }
-  },[productId])
+  }, [productId]);
 
-  if(isLoading){
-    return <div>Loading...</div>
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
 
-  if(error){
-    return <div>
-      Cannot load product details due to {' '}
-      {error instanceof Error ? error.message : 'unknown error'}.
-    </div>
+  if (error) {
+    return (
+      <div>
+        Cannot load product details due to{' '}
+        {error instanceof Error ? error.message : 'unknown error'}.
+      </div>
+    );
   }
 
-  if(!details){
-    return <div>No product details available.</div>
+  if (!details) {
+    return <div>No product details available.</div>;
   }
 
-  const { name, imageUrl, price, color, sizes} = details;
+  const { name, imageUrl, price, color, sizes } = details;
   const colors = {
+    black: '000000',
+    white: 'ffffff',
     grey: '#9ca3af',
     natural: '#fff7ed',
+    brown: '#431407',
     yellow: '#fde047',
     red: '#be123c',
     pink: '#f9a8d4',
@@ -72,25 +77,36 @@ export function Details() {
         <FaRegHeart className="absolute top-5 right-5" />
       </div>
       <div className="w-1/2 mx-8 flex flex-col">
-        <div className="mt-6 mb-3">
-          <p className="py-3">{name}</p>
+        <div className="mt-3 mb-3">
+          <p className="py-3 text-xl">{name}</p>
           <p className="py-3">{toDollars(price)}</p>
-          <hr className="py-3" />
+          <hr className="pb-3" />
         </div>
-        <div className="my-3">
-          <p className="py-3">COLOR: {color}</p>
-          <FaCircle color={colors[color]} className="my-3" />
+        <div className="mb-3">
+          <p className="pb-3">COLOR: {color}</p>
+          {colors[color] === 'white' ? (
+            <FaRegCircle color={colors[color]} className="my-3" />
+          ) : (
+            <FaCircle color={colors[color]} className="my-3" />
+          )}
           <hr className="my-3" />
         </div>
-        <div className="my-6">
+        <div className="mb-2">
           <p className="py-3">SELECT SIZE:</p>
+          <div>
+            {sizes.map((size) => (
+              <button key={size} className="border-solid pl-12 pb-6">
+                {size}
+              </button>
+            ))}
+          </div>
         </div>
         <div className="flex justify-center">
-          <button className="my-6 mx-20 border-solid bg-black text-white w-full h-10">
+          <button className="mx-16 border-solid bg-black text-white w-full h-10">
             Add to bag
           </button>
         </div>
-        <p className="underline my-6 cursor pointer">Product details</p>
+        <p className="underline my-6 cursor-pointer">Product details</p>
       </div>
     </div>
   );
