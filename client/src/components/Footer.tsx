7dc7a7df8dedf49f7aa8a3b811f19d859fd7f9f2
lindsay-jin <lindsayjin1@gmail.com>
2024-05-15 19:02:45 +0000

    added footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="w-screen z-100">
+    <footer className="absolute bottom-0 w-screen z-100">
       <hr className="my-3 border" />
       <div className="px-5 pb-1">
         <p>© Luminelle Inc. All rights reserved.</p>
